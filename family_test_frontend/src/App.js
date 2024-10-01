f24d73c9ebb1fe66b15e183b85ea9bf49fcce5e5
Kaustubh Patil <patilkaustubh990@gmail.com>
2024-10-01 20:13:47 +0530

    frontend-reactDjango

---
+++ family_test_frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import FamilyTestForm from './components/FamilyTestForm';
+// import FamilyTestForm from './components/FamilyTestForm';
+
+function App() {
+    return (
+        <div className="App">
+            <FamilyTestForm />
+        </div>
+    );
+}
+
+export default App;
